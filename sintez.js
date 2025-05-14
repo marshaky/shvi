b99a390cd785184cfcb386e13d81e4d3efea380c
Milena <arshakyanmilena2005@gmail.com>
2025-05-14 16:11:52 +0400

    Rebase

--- sintez.js
+++ sintez.js
@@ -1,4 +1,4 @@
-export { encodeWAV, generatePCM, tokenize };
+export { encodeWAV, generatePCM, tokenize, typeify };
 
 // sample[n]= A ⋅ sin(2 * π * f * (n / R)​)
 
@@ -66,11 +66,15 @@ async function encodeWAV(
 
 const atom = (name) => Symbol.for(name);
 
+const typeify = (token) => {
+  const number = Number.parseFloat(token, 10);
+  return Number.isNaN(number) ? atom(token) : number;
+};
+
 const tokenize = (input) => {
   const array = [];
 
-  if (Array.from(input) == "")
-  {
+  if (Array.from(input) == "") {
     return [];
   }
   const tokens = input.split(" ");
@@ -83,4 +87,14 @@ const tokenize = (input) => {
     }
   });
   return array;
+};
+
+const run = (
+  program,
+  definitions = [],
+) => {
+  const tokens = tokenize(program);
+  const value = evaluate(tokens, definitions);
+
+  return value;
 };
